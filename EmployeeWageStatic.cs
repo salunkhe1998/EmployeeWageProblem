5a0aec9b3b90f2027eca66865a02fe4b674d89d3
Ashwini <salunkheashwini1998@gmail.com>
2023-01-06 16:35:25 +0530

    this is file

--- EmployeeWageStatic.cs
+++ EmployeeWageStatic.cs
@@ -14,35 +14,44 @@ namespace EmployeeWageProblem
             int Is_Full_Time = 1;
             int Is_Part_Time = 2;
             int Wage_Per_Hour = 20;
+            int Working_Days_IN_Month = 20;
             int empHour;
-            int DailyEmpWage;
-            Random R = new Random();
-            int empCheck = R.Next(3);
+            int MonthlyEmpWages;
+            int TotalHrs = 0;
 
-            switch (empCheck)
+
+            for (int i = 0; i < Working_Days_IN_Month; i++)
             {
-                case 1:
-                    {
-                        Console.WriteLine("Employee is present.");
-                        empHour = 8;
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.WriteLine("Employee present PartTime.");
-                        empHour = 4;
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine("Employee is absent.");
-                        empHour = 0;
-                        break;
-                    }
-            }
+                Random R = new Random();
+                int empCheck = R.Next(3);
+                switch (empCheck)
+                {
+                    case 1:
+                        {
+                            //  Console.WriteLine("Employee is present.");
+                            empHour = 8;
+                            break;
+                        }
+                    case 2:
+                        {
+                            // Console.WriteLine("Employee present PartTime.");
+                            empHour = 4;
+                            break;
+                        }
+                    default:
+                        {
+                            //  Console.WriteLine("Employee is absent.");
+                            empHour = 0;
+                            break;
+                        }
 
-            DailyEmpWage = Wage_Per_Hour * empHour;
-            Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
+                }
+
+                TotalHrs = TotalHrs + empHour;
+            }
+            Console.WriteLine("Total working hours in month are :- " + TotalHrs);
+            MonthlyEmpWages = TotalHrs * Wage_Per_Hour;
+            Console.WriteLine("Monthly employee wage is :- " + MonthlyEmpWages);
         }
     }
 }
