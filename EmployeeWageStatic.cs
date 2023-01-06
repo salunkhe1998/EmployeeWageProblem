4aadf64ee959f17735318c4827bef4fd57de4d8d
Ashwini <salunkheashwini1998@gmail.com>
2023-01-06 16:20:15 +0530

    this is file

--- EmployeeWageStatic.cs
+++ EmployeeWageStatic.cs
@@ -11,16 +11,22 @@ namespace EmployeeWageProblem
         public static void Employee()
         {
             int Is_Full_Time = 1;
+            int Wage_Per_Hour = 20;
+            int FullDayHour = 8;
             Random R = new Random();
             int empCheck = R.Next(2);
             if (empCheck == Is_Full_Time)
             {
                 Console.WriteLine("Employee is present.");
+                FullDayHour = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent.");
+                FullDayHour = 0;
             }
+            int DailyEmpWage = Wage_Per_Hour * FullDayHour;
+            Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
         }
     }
 }
