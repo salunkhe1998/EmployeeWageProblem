9bbd48b0d326246f286552fb521d51c213d2743a
Ashwini <salunkheashwini1998@gmail.com>
2023-01-06 16:16:10 +0530

    This is file

---
+++ EmployeeWageStatic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeWageStatic
+    {
+        public static void Employee()
+        {
+            int Is_Full_Time = 1;
+            Random R = new Random();
+            int empCheck = R.Next(2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+        }
+    }
+}
