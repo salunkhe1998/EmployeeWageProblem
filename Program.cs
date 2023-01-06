d94acd5f358b192457ac992948bbdf4c210e795a
Ashwini <salunkheashwini1998@gmail.com>
2023-01-06 16:06:32 +0530

    This is file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageNonStatic
+{
+    public class Method
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("***** Welcome to the employee wage computation Program for Static Methods. *****");
+
+        }
+    }
+}
